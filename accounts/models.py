1edc2fbd59c254bbab0424980a91ac8d081870ba
Tom <tomverberk@gmail.com>
2019-10-21 18:34:02 +0200

    connect users

--- accounts/models.py
+++ accounts/models.py
@@ -37,7 +37,7 @@ class Module(models.Model):
 class Module_User(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
-    currentModule = models.BooleanField(default = False)
+    currentModule = models.IntegerField(default = 0)
     amountCorrect = models.IntegerField(default = 0)
     amountWrong = models.IntegerField(default = 0)
     amountHints = models.IntegerField(default = 0)
